ba9be9566f527b3111b10da575d1cdc4d13be7ff
arminEX <stob5r11@gmail.com>
2024-11-20 21:22:48 +0330

    components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+class Timer extends React.Component{
+  render(){
+    return(
+      <h2>
+        it's {new Date().toLocaleTimeString()}
+      </h2>
+    )
+  }
+} 
+
+class Hello extends React.Component{
+  render(){
+    return(
+      <h1>hi friends</h1>
+    )
+  }
+}
+
 const tick = () => {
   const element = (
     <div>
-      <h1>hi friends</h1>
-      <h2>it's {new Date().toLocaleTimeString()}</h2>
+      <Timer/>
+      <Hello/>
     </div>
   );
   root.render(element);
@@ -14,6 +32,3 @@ const tick = () => {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 tick();
 setInterval(tick, 1000);
-
-// test
-//test2
