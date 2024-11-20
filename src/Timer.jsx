02ce3ab2e0e7c2f78b5f84f8994ef92f7443b358
arminEX <stob5r11@gmail.com>
2024-11-21 00:34:11 +0330

    react life cycl

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+var interval;
 class Timer extends React.Component{
     constructor(){
       super();
@@ -7,12 +8,27 @@ class Timer extends React.Component{
         time : new Date().toLocaleTimeString()
       }
     }
-    render(){
-      setInterval(() => {
+    componentDidMount(){
+        interval = setInterval(() => {
         this.setState({
           time : new Date().toLocaleTimeString()
         })
       } ,1000)
+    }
+
+    componentDidUpdate(){
+        console.log("componentDidUpdate")
+
+        if(this.state.time === "12:32:25 AM"){
+            clearInterval(interval);
+        }
+    }
+
+    componentWillUnmount(){
+        console.log("componentWillUnmount")
+    }
+    render(){
+      
       return(
         <h2 className='timer'>
           it's {this.state.time}
