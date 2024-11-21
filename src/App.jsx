f7436e9c567106b986aa4049cd2d17649d593b1d
arminEX <stob5r11@gmail.com>
2024-11-21 13:05:53 +0330

    hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Timer from './Timer';
 import Hello from './Hello';
 
-class App extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            title : "سلام دوستان عزیز"
-        }
-    }
 
-    handelSetState =()=>{
-        this.setState({
-            title : "به react خوش آمدید"
-        })
-    }
-  render() {
-    return (
-      <div className="main">
-        <Hello title={this.state.title}/>
-        <Timer x={this.handelSetState}/>
-      </div>
-    );
+const App =()=>{
+  const[title, setTitle] = useState("سلام دوستان عزیز")
+  const handelSetState =()=>{
+    setTitle("به react خوش آمدید")
   }
+
+  return (
+    <div className="main">
+      <Hello title={title}/>
+      <Timer x={handelSetState}/>
+    </div>
+  );
+
 }
+
+
+
+// class App extends React.Component {
+//     constructor(){
+//         super();
+//         this.state = {
+//             title : "سلام دوستان عزیز"
+//         }
+//     }
+
+//     handelSetState =()=>{
+//         this.setState({
+//             title : "به react خوش آمدید"
+//         })
+//     }
+//   render() {
+//     return (
+//       <div className="main">
+//         <Hello title={this.state.title}/>
+//         <Timer x={this.handelSetState}/>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
 
-
-
-// import React from "react";
-// import Timer from './Timer';
-// import Hello from './Hello';
-// import ReactDOM from 'react-dom/client';
-
-// const App = () => {
-//     const element = (
-//       <div className='main'>
-//         <Hello/>
-//         <Timer/>
-//       </div>
-//     );
-//     root.render(element);
-//   };
-//   const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// export default App;
